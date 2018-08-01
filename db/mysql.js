647f784f124363ce4640764314914bab559864a2
nunocourela <nuno.courela@nmbrs.nl>
2018-08-01 22:56:43 +0100

    Uppercase environment variables

--- db/mysql.js
+++ db/mysql.js
@@ -3,8 +3,8 @@ const mysql = require('mysql');
 function ping() {
     var con = mysql.createConnection({
         host: "sabaik6fx8he7pua.chr7pe7iynqr.eu-west-1.rds.amazonaws.com",
-        user: process.env.mysql_username,
-        password: process.env.mysql_password
+        user: process.env.MYSQL_USERNAME,
+        password: process.env.MYSQL_PASSWORD
     });
 
     con.connect(function (err) {
