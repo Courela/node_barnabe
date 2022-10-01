5450c536ed0f22e16920b449daaed5013e63ead1
Courela <courela@gmail.com>
2022-10-01 18:07:49 +0100

    Settings built differently

--- settings.js
+++ settings.js
@@ -1,13 +1,25 @@
 var settings = process.env.NODE_ENV !== 'production' ? 
-    { API_PROTOCOL: 'http', API_HOST: 'localhost', API_PORT: '8001' } :
-    { API_PROTOCOL: 'https', API_HOST: process.env.APP_HOST, API_PORT: '443' };
-
-settings = Object.assign(settings, 
-    { API_URL: settings.API_PROTOCOL + '://' + settings.API_HOST + ':' + settings.API_PORT,
+    { 
+        API_PROTOCOL: 'http', 
+        API_HOST: 'localhost', 
+        API_PORT: '8001',
+        API_URL: '',
         DEFAULT_TABLE_PAGE_SIZE: 20,
         PLAYER_REQUEST_TIMEOUT: 300000,
-        CORS_HOST: process.env.NODE_ENV !== 'production' ? '*' : settings.API_PROTOCOL + '://' + settings.API_HOST
-    });
+        CORS_HOST: ''
+    } :
+    { 
+        API_PROTOCOL: 'https', 
+        API_HOST: process.env.APP_HOST, 
+        API_PORT: '443' ,
+        API_URL: '',
+        DEFAULT_TABLE_PAGE_SIZE: 20,
+        PLAYER_REQUEST_TIMEOUT: 300000,
+        CORS_HOST: ''
+    };
+
+settings.CORS_HOST = process.env.NODE_ENV !== 'production' ? '*' : settings.API_PROTOCOL + '://' + settings.API_HOST;
+settings.API_URL = settings.API_PROTOCOL + '://' + settings.API_HOST + ':' + settings.API_PORT;
 // settings.PLAYER_RESPONSE_TIMEOUT = 300;
 
 module.exports = {
