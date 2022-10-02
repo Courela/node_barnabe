696a2bd1a49549aa14d1b8434371c797b2e64970
Courela <courela@gmail.com>
2022-10-02 12:31:02 +0100

    UI: REACT_APP variables on .env file; serverSettings with environment variables

---
+++ serverSettings.js
@@ -0,0 +1,27 @@
+var serverSettings = process.env.NODE_ENV !== 'production' ? 
+    { 
+        API_PROTOCOL: 'http', 
+        API_HOST: process.env.APP_HOST, 
+        API_PORT: '8001',
+        API_URL: '',
+        DEFAULT_TABLE_PAGE_SIZE: 20,
+        PLAYER_REQUEST_TIMEOUT: 300000,
+        CORS_HOST: ''
+    } :
+    { 
+        API_PROTOCOL: 'https', 
+        API_HOST: process.env.APP_HOST, 
+        API_PORT: '443' ,
+        API_URL: '',
+        DEFAULT_TABLE_PAGE_SIZE: 20,
+        PLAYER_REQUEST_TIMEOUT: 300000,
+        CORS_HOST: ''
+    };
+
+serverSettings.CORS_HOST = process.env.NODE_ENV !== 'production' ? '*' : serverSettings.API_PROTOCOL + '://' + serverSettings.API_HOST;
+serverSettings.API_URL = serverSettings.API_PROTOCOL + '://' + serverSettings.API_HOST + ':' + serverSettings.API_PORT;
+// settings.PLAYER_RESPONSE_TIMEOUT = 300;
+
+module.exports = {
+     settings: serverSettings
+}
