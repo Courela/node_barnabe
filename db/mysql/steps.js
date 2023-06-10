73c9d3569c9728ae2488ebd86da30c73579758a0
Courela <courela@gmail.com>
2023-06-10 13:37:55 +0100

    MySQL2 package

--- db/mysql/steps.js
+++ db/mysql/steps.js
@@ -1,4 +1,4 @@
-const mysql = require('mysql');
+const mysql = require('mysql2');
 const adapter = require('../mysql');
 
 function insertMatch(season, stepId, phaseId, homeTeamId, awayTeamId, homeTeamGoals, awayTeamGoals, callback) {
